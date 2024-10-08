4682c3feafb0f3b37a09b2db7c4ce1107dcb005f
ALFREDO MALDONADO <alfredorafael.maldonado@alumno.iesluisvives.org>
2024-10-08 18:01:39 +0200

    activities detching, insertion added

--- app/src/main/java/com/example/firebaserealtimekotlin/InsertionActivity.kt
+++ app/src/main/java/com/example/firebaserealtimekotlin/InsertionActivity.kt
@@ -1,36 +1,20 @@
 package com.example.firebaserealtimekotlin
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var btnInsertData: Button
-    private lateinit var btnFetchData: Button
+class InsertionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_insertion)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-        btnInsertData = findViewById(R.id.btnInsertData)
-        btnFetchData = findViewById(R.id.btnFetchData)
-
-        btnInsertData.setOnClickListener {
-            val intent = Intent(this, InsertionActivity::class.java)
-            startActivity(intent)
-        }
-
-        btnFetchData.setOnClickListener {
-            val intent = Intent(this, FetchingActivity::class.java)
-            startActivity(intent)
         }
     }
 }
